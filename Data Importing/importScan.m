6279cdf4948eb6ae34d6c05bdfc38cc0a330d12e
Matthew Dvorsky <mdvorsky@iastate.edu>
2021-10-20 12:58:54 -0500

    Updated "importScan.m" to be more robust and to give warning when missing data.

--- Data Importing/importScan.m
+++ Data Importing/importScan.m
@@ -26,73 +26,117 @@ else
     filename = filenameIn;
 end
 
+% Don't forget to close the fileHandle whenever returning or throwing.
 fileHandle = fopen(filename);
 if fileHandle == -1
-    error("File ('%s') not found.", filename);
+    error("Scan file '%s' not found.", filename);
 end
 
 %% Get Version Info
-scanFileCode = fread(fileHandle, 1, 'double');
+% Scan file code is an value contained in the first 8 bytes of the file.
+scanFileCode = fread(fileHandle, 1, "double");
 if scanFileCode ~= 63474328
     fclose(fileHandle);
     error("*.scan file type not recognized.");
 end
 
-scanFileVersion = fread(fileHandle, 1, 'double');
+% Currently, there is only 1 version of the scan file format.
+scanFileVersion = fread(fileHandle, 1, "double");
 if scanFileVersion ~= 1
     fclose(fileHandle);
     error("*.scan file version is not supported.");
 end
 
-%% Get Header Data
+%% Read Remaining Header Data From ".scan" File
 Header.header = (string(fread(fileHandle, ...
-    fread(fileHandle, 1, 'double'), 'double=>char').'));
+    fread(fileHandle, 1, "double"), "double=>char").'));
 Header.description = (string(fread(fileHandle, ...
-    fread(fileHandle, 1, 'double'), 'double=>char').'));
+    fread(fileHandle, 1, "double"), "double=>char").'));
 Header.deviceName = (string(fread(fileHandle, ...
-    fread(fileHandle, 1, 'double'), 'double=>char').'));
+    fread(fileHandle, 1, "double"), "double=>char").'));
 
-%% Read data from .scan file
-isUniform = fread(fileHandle, 1, 'double');
+isUniform = fread(fileHandle, 1, "double");
 Header.isUniform = isUniform;
-numDims = fread(fileHandle, 1, 'double');
-axisOrder = fread(fileHandle, numDims, 'double') + 1;
-numSteps = fread(fileHandle, numDims, 'double');
-numChannels = fread(fileHandle, 1, 'double');
+numDims = fread(fileHandle, 1, "double");
+axisOrder = 1 + fread(fileHandle, numDims, "double");
+numSteps = fread(fileHandle, numDims, "double");
+numChannels = fread(fileHandle, 1, "double");
 
 Header.channelNames = strings(numChannels, 1);
 for ii = 1:numChannels
     Header.channelNames(ii) = (string(fread(...
-        fileHandle, fread(fileHandle, 1, 'double'), 'double=>char').'));
+        fileHandle, fread(fileHandle, 1, "double"), "double=>char").'));
 end
 
-numF = fread(fileHandle, 1, 'double');
-isComplex = fread(fileHandle, 1, 'double');
+numF = fread(fileHandle, 1, "double");
+isComplex = fread(fileHandle, 1, "double");
 
 axisCoordinates = cell(numDims, 1);
 if isUniform
-    for ii = 1:numDims  % Get relative location
-        axisCoordinates(ii) = {fread(fileHandle, numSteps(ii), 'double')};
+    % In the uniform case, numSteps(n) will contain the size of the nth
+    % dimension. The file will contain numSteps(n) data points for each
+    % dimension, specifying the relative coordinates of each uniform grid.
+    % Then, the absolute coordinates are specified in the same way. The
+    % absolute coordinates are currently ignored.
+    for ii = 1:numDims  % Get relative location vector
+        axisCoordinates(ii) = {fread(fileHandle, numSteps(ii), "double")};
     end
-    fread(fileHandle, sum(numSteps), 'double'); % Discard absolute location
+    fread(fileHandle, sum(numSteps), "double"); % Discard absolute location
 else
+    % In the nonuniform case, numSteps(1) will contain the total number of
+    % scan points. The file will contain numSteps(1) data points for each
+    % dimension, specifying the relative coordinates of each every scan
+    % point grid. Then, the absolute coordinates are specified in the same
+    % way. The absolute coordinates are currently ignored.
     for ii = 1:numDims  % Get relative location
-        axisCoordinates(ii) = {fread(fileHandle, numSteps(1), 'double')};
+        axisCoordinates(ii) = {fread(fileHandle, numSteps(1), "double")};
     end
-    fread(fileHandle, numSteps(1) .* 3, 'double'); % Discard absolute location
+    fread(fileHandle, numSteps(1) .* 3, "double"); % Discard absolute location
 end
 
-F = fread(fileHandle, numF, 'double'); % Read frequencies
+% Next, the file contains the vector of frequencies in GHz
+F = fread(fileHandle, numF, "double"); % Read frequency vector
 
+% If end of file was reached, header is missing data.
+if feof(fileHandle)
+    fclose(fileHandle);
+    error("Scan file '%s' header is corrupted.", filename);
+end
+
+%% Read Scan Measurement Data
 numDataPoints = numF .* numChannels .* prod(numSteps);
+
+% Read data from file.
+numDataPointsToRead = numDataPoints * (1 + isComplex);
+Data = fread(fileHandle, numDataPointsToRead, "double");
+
+% Check for missing data
+if length(Data) < numDataPointsToRead
+    warning(strcat("Scan file '%s' contains (%d) measurement points, ", ...
+        "which is less than the (%d) measurement points expected. ", ...
+        "Data will be padded with zeros."), ...
+        filename, length(Data), numDataPointsToRead);
+    
+    % Pad Missing Data With Zeros
+    Data = [Data; zeros(numDataPointsToRead - length(Data), 1)];
+end
+
+% Check for too much data
+fread(fileHandle, 1);
+if ~feof(fileHandle)
+    warning(strcat("Scan file '%s' contains more data than expected. ", ...
+        "The extra data will be ignored."), filename);
+end
+
+% Reorganize complex data.
 if isComplex
-    Data = fread(fileHandle, 2 .* numDataPoints, 'double');
+    % Complex data is partially interleaved real imaginary. Every scan
+    % point contains all real data followed by all imaginary data.
     Data = reshape(Data, numF .* numChannels, 2, []);
     Data = complex(Data(:, 1, :), Data(:, 2, :));
-else
-    Data = fread(fileHandle, numDataPoints, 'double');
 end
 
+% Done with reading data from file.
 fclose(fileHandle);
 
 %% Reorganize data into proper format
