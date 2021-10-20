57c1b4ea6ae4293319a63287c16e22ba79ee729b
Matthew Dvorsky <mdvorsky@iastate.edu>
2021-10-20 11:43:12 -0500

    Modified "importScan.m" to support filenames with no extension, by adding ".scan" automatically if needed. Also, added documentation.

--- Data Importing/importScan.m
+++ Data Importing/importScan.m
@@ -1,80 +1,99 @@
-function [X, Y, Z, F, Data, Header] = importScan(Filepath)
+function [X, Y, Z, F, Data, Header] = importScan(filenameIn)
+%IMPORTSCAN Import a ".scan" file created by the amntl scanner program.
+% This function imports and parses a ".scan" file.
+%
+% Example Usage:
+%   [X, Y, Z, F, Data, Header] = importScan(filename);
+%
+% Inputs:
+%   filenameIn - Path to the file. If the file has no extension, ".scan"
+%       will be automatically appended.
+% Output:
+%   X, Y, Z, ... - Vectors containing the coordinates of each scan point.
+%       If the scan is uniform, each vector will contain a unique, sorted,
+%       and uniformly increasing list of coordinates for the corresponding
+%       dimension. Otherwise, each vector will contain one element for each
+%       scan point.
+%
+% Author: Matt Dvorsky
 
-X = [];
-Y = [];
-Z = [];
-F = [];
-Data = [];
+%% Open File
+% Add ".scan" extension if filenameIn has no extension.
+[path, name, ext] = fileparts(filenameIn);
+if (ext == "")
+    filename = fullfile(path, strcat(name, ".scan"));
+else
+    filename = filenameIn;
+end
 
-File = fopen(Filepath);
-
-if File == -1
-    error("File not found.");
+fileHandle = fopen(filename);
+if fileHandle == -1
+    error("File ('%s') not found.", filename);
 end
 
 %% Get Version Info
-scanFileCode = fread(File, 1, 'double');
+scanFileCode = fread(fileHandle, 1, 'double');
 if scanFileCode ~= 63474328
-    fclose(File);
+    fclose(fileHandle);
     error("*.scan file type not recognized.");
 end
 
-scanFileVersion = fread(File, 1, 'double');
+scanFileVersion = fread(fileHandle, 1, 'double');
 if scanFileVersion ~= 1
-    fclose(File);
+    fclose(fileHandle);
     error("*.scan file version is not supported.");
 end
 
 %% Get Header Data
-Header.header = (string(fread(File, ...
-    fread(File, 1, 'double'), 'double=>char').'));
-Header.description = (string(fread(File, ...
-    fread(File, 1, 'double'), 'double=>char').'));
-Header.deviceName = (string(fread(File, ...
-    fread(File, 1, 'double'), 'double=>char').'));
+Header.header = (string(fread(fileHandle, ...
+    fread(fileHandle, 1, 'double'), 'double=>char').'));
+Header.description = (string(fread(fileHandle, ...
+    fread(fileHandle, 1, 'double'), 'double=>char').'));
+Header.deviceName = (string(fread(fileHandle, ...
+    fread(fileHandle, 1, 'double'), 'double=>char').'));
 
 %% Read data from .scan file
-isUniform = fread(File, 1, 'double');
+isUniform = fread(fileHandle, 1, 'double');
 Header.isUniform = isUniform;
-numDims = fread(File, 1, 'double');
-axisOrder = fread(File, numDims, 'double') + 1;
-numSteps = fread(File, numDims, 'double');
-numChannels = fread(File, 1, 'double');
+numDims = fread(fileHandle, 1, 'double');
+axisOrder = fread(fileHandle, numDims, 'double') + 1;
+numSteps = fread(fileHandle, numDims, 'double');
+numChannels = fread(fileHandle, 1, 'double');
 
 Header.channelNames = strings(numChannels, 1);
 for ii = 1:numChannels
     Header.channelNames(ii) = (string(fread(...
-        File, fread(File, 1, 'double'), 'double=>char').'));
+        fileHandle, fread(fileHandle, 1, 'double'), 'double=>char').'));
 end
 
-numF = fread(File, 1, 'double');
-isComplex = fread(File, 1, 'double');
+numF = fread(fileHandle, 1, 'double');
+isComplex = fread(fileHandle, 1, 'double');
 
 axisCoordinates = cell(numDims, 1);
 if isUniform
     for ii = 1:numDims  % Get relative location
-        axisCoordinates(ii) = {fread(File, numSteps(ii), 'double')};
+        axisCoordinates(ii) = {fread(fileHandle, numSteps(ii), 'double')};
     end
-    fread(File, sum(numSteps), 'double'); % Discard absolute location
+    fread(fileHandle, sum(numSteps), 'double'); % Discard absolute location
 else
     for ii = 1:numDims  % Get relative location
-        axisCoordinates(ii) = {fread(File, numSteps(1), 'double')};
+        axisCoordinates(ii) = {fread(fileHandle, numSteps(1), 'double')};
     end
-    fread(File, numSteps(1) .* 3, 'double'); % Discard absolute location
+    fread(fileHandle, numSteps(1) .* 3, 'double'); % Discard absolute location
 end
 
-F = fread(File, numF, 'double'); % Read frequencies
+F = fread(fileHandle, numF, 'double'); % Read frequencies
 
 numDataPoints = numF .* numChannels .* prod(numSteps);
 if isComplex
-    Data = fread(File, 2 .* numDataPoints, 'double');
+    Data = fread(fileHandle, 2 .* numDataPoints, 'double');
     Data = reshape(Data, numF .* numChannels, 2, []);
     Data = complex(Data(:, 1, :), Data(:, 2, :));
 else
-    Data = fread(File, numDataPoints, 'double');
+    Data = fread(fileHandle, numDataPoints, 'double');
 end
 
-fclose(File);
+fclose(fileHandle);
 
 %% Reorganize data into proper format
 if numDims == 3
