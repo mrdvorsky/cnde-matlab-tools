e9e0ebb4c127d8a2a2f53a564b001b911d5718ca
Matthew Dvorsky <mdvorsky@iastate.edu>
2021-10-26 17:23:31 -0500

    Updated documentation for clenshawCurtis integration routines.

--- Integration/clenshawCurtis.m
+++ Integration/clenshawCurtis.m
@@ -1,57 +1,74 @@
-function [ nodes, weights ] = clenshawCurtis( orderN, a, b, w )
-%CLENSHAWCURTIS Generate integration weights and nodes for closed interval
-%   This function generates the weights and nodes required to compute a
-%   definite integral over a closed interval. The weights and nodes are
-%   defined using the Clenshaw-Curtis Quadrature rules.
-%   
-%   The function outputs "nodes" and "weights" can be used to approximate
-%   the definite integral of a function f(x)dx over the interval [a,b] by
-%   computing I = sum(weights .* f(nodes)). This should give approximately
-%   the same result as I = integral(f, a, b), with a higher value of
-%   orderN resulting in a better approximation. The parameter orderN is
-%   the number of points at which to evaluate f(x). If f(x) is a polynomial
-%   with degree less than or equal to orderN, the result will be exact.
-%   The inputs "a" and "b" must be real, finite values.
+function [nodes, weights] = clenshawCurtis(orderN, a, b, options)
+%CLENSHAWCURTIS Generate Clenshaw-Curtiss weights and nodes for closed interval integration.
+% This function generates the weights and nodes required to compute a
+% definite integral over a closed interval. The weights and nodes are
+% defined using the Clenshaw-Curtis Quadrature rules.
 %
-%   A weighting function "w" can be optionally supplied such that the
-%   integral I = sum(weights .* f(nodes)) corresponds to the integral of
-%   f(x)w(x)dx over the closed interval [a,b]. In this case, the value I
-%   will be exact f(x) is a polynomial with degree less than orderN,
-%   regardless of w(x). The input "w" should be a function handle that
-%   accepts a scalar input and returns a scalar output. The default value
-%   of "w" is effectively w(x) = 1.
+% The function outputs "nodes" and "weights" can be used to approximate
+% the definite integral of a function f(x)dx over the interval [a,b] by
+% computing q = sum(weights .* f(nodes)). This should give approximately
+% the same result as q = integral(f, a, b), with a higher value of
+% orderN resulting in a better approximation.
+%
+% If f(x) is a polynomial with degree less than orderN, the result will be
+% exact. Use of these quadrature rules will result in evalation of the
+% function at the interval endpoints a and b.
+%
+% A weighting function "w" can be optionally supplied such that the
+% integral 1 = sum(weights .* f(nodes)) corresponds to the integral of
+% f(x)w(x)dx over the closed interval [a,b]. In this case, the value 1
+% will be exact f(x) is a polynomial with degree less than orderN,
+% regardless of w(x). The input "w" should be a function handle that
+% accepts a scalar input and returns a scalar output. The default value
+% of "w" is effectively w(x) = 1. The input "w" is specified using the
+% WeightingFunction named argument.
+%
+% Example Usage:
+%   [nodes, weights] = clenshawCurtis(N, a, b);
+%   [nodes, weights] = clenshawCurtis(N, a, b, WeightingFunction=fun);
+%   [nodes, weights] = clenshawCurtis(N, a, b, ComputeErrorWeights=false);
+%   q = sum(fun(nodes) .* weights, 1);
+%
+% Inputs:
+%   orderN - Scalar number of nodes to calculate.
+%   a - Scalar integration lower bound. Must be finite.
+%   b - Scalar integration upper bound. Must be finite.
+% Outputs:
+%   nodes - Column vector of coordinates at which to evaluate function.
+%   weights - Column vector of weights to perform weighted sum.
+%   errorWeights - Column vector of weights to estimate integration error.
+% Named Options:
+%   WeightingFunction - Optional weighting function w(x). See above.
+%
+% Author: Matt Dvorsky
 
-%% Check Input
-if nargin < 3
-    b = 1;
-end
-if nargin < 2
-    a = -1;
-end
-if nargin < 1
-    orderN = 10;
+arguments
+   orderN(1, 1) {mustBeInteger, mustBeFinite} = 10;
+    a(1, 1) {mustBeNumeric, mustBeFinite} = -1;
+    b(1, 1) {mustBeNumeric, mustBeFinite} = 1;
+    options.WeightingFunction;
 end
 
+%% Calculate Moments
 % Round orderN up to nearest even integer
 N = 2*ceil(0.5*orderN);
 
-%% Calculate Indices
 n(:, 1) = 0:N;
-if nargin < 4
-    d = 2 ./ (1 - n.^2);
-    d(2:2:end) = 0;
-else
-    d = 0*n;
-    for ii = 1:length(d)
-        d(ii) = integral(...
-            @(x) w(0.5*(b - a) .* cos(x) + 0.5*(a + b)) ...
+if isfield(options, "WeightingFunction")
+    moments = zeros(size(n));
+    for ii = 1:length(moments)
+        moments(ii) = integral(...
+            @(x) options.WeightingFunction(0.5*(b - a) .* cos(x) + 0.5*(a + b)) ...
             .* sin(x) .* cos((ii - 1)*x), 0, pi);
     end
+else
+    moments = 2 ./ (1 - n.^2);
+    moments(2:2:end) = 0;
 end
 
 %% Calculate Weights using DCT Type 1
 % Use even ifft to calculate DCT-1.
-weights = real(ifft([d; d(end - 1:-1:2)]));
+weights = real(ifft([moments; moments(end - 1:-1:2)]));
 weights = weights(1:N + 1);
 weights(2:end - 1) = 2*weights(2:end - 1);
 
