4fdcc3949c756161be9e642d1fa33a32f1ea4eb6
Matthew Dvorsky <mdvorsky@iastate.edu>
2022-02-22 14:31:03 -0600

    Updated documentation of "createSarData3d.m".

--- SAR Utilities/createSarData3d.m
+++ SAR Utilities/createSarData3d.m
@@ -1,31 +1,42 @@
 function [S] = createSarData3d(x, y, f, x0, y0, z0, a0, options)
-%CREATESARDATA3D Create uniform SAR data from point source specificication
-%   Example Usage:
-%       S = createSarData3d(x, y, f, x0, y0, z0);
-%       S = createSarData3d(x, y, f, x0, y0, z0, a0);
-%       S = createSarData3d(x, y, f, x0, y0, z0, UseRangeForAmplitude=false);
-%       S = createSarData3d(x, y, f, x0, y0, z0, a0, SpeedOfLight=299.78e6);
-%   
-%   This function returns uniform SAR measurement data that would have
-%   occured measuring point targets located at x0(ii), y0(ii), z0(ii)
-%   with scattering coefficients a0(ii). The parameter pAmp is optional
-%   and will default to ones(size(px));
+%CREATESARDATA3D Create uniform SAR data from point source specificication.
+% This function returns uniform SAR measurement data that would have
+% occured measuring point targets located at x0(ii), y0(ii), z0(ii)
+% with scattering coefficients a0(ii).
 %
-%   The output S will be of size length(x) by length(y) by length(f), each
-%   value correspoding to the measurement made at the coordinate and
-%   frequency corresponding to the same indices in the inputs x, y, f.
+% Example Usage:
+%   S = createSarData3d(x, y, f, x0, y0, z0);
+%   S = createSarData3d(x, y, f, x0, y0, z0, a0);
+%   S = createSarData3d(x, y, f, x0, y0, z0, UseRangeForAmplitude=false);
+%   S = createSarData3d(x, y, f, x0, y0, z0, a0, SpeedOfLight=299.78e6);
+% 
+% The output S will be of size length(x) by length(y) by length(f), each
+% value correspoding to the measurement made at the coordinate and
+% frequency corresponding to the same indices in the inputs x, y, f.
 %
-%   The default units are mm and GHz, but this can be changed by specifying
-%   the speed of light as an optional named parameter.
+% The default units are mm and GHz, but this can be changed by specifying
+% the speed of light as an optional named parameter.
 %
-%   Additional options can be provided by specifying optional named
-%   parameters. These named parameters are listed below.
-%       UseRangeForAmplitude (true) -> specifies whether or not to include
-%           the 1/R^2 term to scale the magnitude of the output.
-%       SpeedOfLight (299.792458) -> value of speed of light to use.
-%       thetaBeamwidthX (inf) -> halfpower beamwidth of the antenna in the
-%           xz-plane in radians.
-%       thetaBeamwidthX (inf) -> same as thetaBeamwidthX for the yz-plane.
+% Inputs:
+%   x - Vector of sample x-coordinates.
+%   y - Vector of sample y-coordinates.
+%   f - Vector of sample frequency coordinates.
+%   x0 - Vector of point target x-coordinates.
+%   y0 - Vector of point target y-coordinates. Must be same length as x0.
+%   z0 - Vector of point target z-coordinates. Must be same length as x0.
+%   a0 (optional) - Vector of point target reflectivities. Must be same
+%       length as x0. Defaults to ones(size(x0));
+% Outputs:
+%   S - Matrix of size length(x) by length(y) by length(f), each value
+%       correspoding to the measurement made at the coordinate and
+%       frequency corresponding to the same indices in the inputs x, y, f.
+% Named Arguments:
+%   UseRangeForAmplitude (true) - Specifies whether or not to include the
+%       1/R^2 term to scale the magnitude of the output.
+%   SpeedOfLight (299.792458) - Value of speed of light to use.
+%   ThetaBeamwidthX (inf) - Halfpower beamwidth of the antenna in the
+%       xz-plane in radians.
+%   ThetaBeamwidthY (inf) - Same as thetaBeamwidthX for the yz-plane.
 %
 % Author: Matt Dvorsky
 
@@ -38,11 +49,11 @@ arguments
     z0(:, 1);
     a0(:, 1) = ones(size(x0));
     options.UseRangeForAmplitude = true;
-    options.SpeedOfLight = 299.792458;
-    options.ThetaBeamwidthX = inf;
-    options.ThetaBeamwidthY = inf;
-    options.Er = 1;
-    options.Thk = inf;
+    options.SpeedOfLight(1, 1) = 299.792458;
+    options.ThetaBeamwidthX(1, 1) = inf;
+    options.ThetaBeamwidthY(1, 1) = inf;
+    options.Er(:, 1) = 1;
+    options.Thk(:, 1) = inf;
 end
 
 %% Create SAR Data
