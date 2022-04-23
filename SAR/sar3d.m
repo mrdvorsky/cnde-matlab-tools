db45bcab47ede470386d81baf424c430157f3f36
Matthew Dvorsky <mdvorsky@iastate.edu>
2022-04-22 19:28:55 -0500

    Updated documentation for sar3d.

--- SAR/sar3d.m
+++ SAR/sar3d.m
@@ -1,17 +1,59 @@
 function [Image] = sar3d(S, x, y, z, f, options)
-%SAR3D Formulate Plane Wave SAR image from raw data
-%   Usage:
-%       Img = sar3d(S, x, y, z, f);
-%       Img = sar3d(S, x, y, z, f, ZeroPadPercent=25);
-%       Img = sar3d(S, x, y, z, f, ZeroPadPercentX=25, ZeroPadPercentY=0);
-%       Img = sar3d(S, x, y, z, f, er=[2, 3], thk=[10, inf]); % Not implemented
+%SAR3D Calculate SAR image(s) from a uniform 2D scan data set or sets.
+% This function returns a 3D complex SAR image given measurement data from
+% a 2D frequency-stepped xy-raster scan. Optionally, can process multiple
+% data sets having the same dimensions simultaneously.
 %
-%   S -> Raw measurement data at each location and frequency. Should be of
-%        size length(x) by length(y) by length(f)
-%   x -> Vector of x coordinates (mm)
-%   y -> Vector of y coordinates (mm)
-%   z -> Vector of desired z coordinates (mm)
-%   f -> Vector of freqeuncy coordinates (GHz)
+% Example Usage:
+%   Img = sar3d(S, x, y, z, f);
+%   Img = sar3d(S, x, y, z, f, ZeroPadPercent=25);
+%   Img = sar3d(S, x, y, z, f, ZeroPadPercentX=25, ZeroPadPercentY=0);
+%   Img = sar3d(S, x, y, z, f, RemoveAverage=false);
+%   Img = sar3d(S, x, y, z, f, Er=[2, 3], Thk=[10, inf]);
+%
+% The output Img will have size length(x) by length(y) by length(z) by ...,
+% each value correspoding to the measurement made at the coordinate
+% corresponding to the same indices in the inputs x, y, z, with extra
+% dimensions (4 and onward) being preserved in the output. Each image
+% Img(:, :, :, ii) will be a SAR image of the data S(:, :, :, ii).
+%
+% The default units are mm and GHz, but this can be changed by specifying
+% the speed of light as an optional named parameter.
+%
+% Inputs:
+%   S - Matrix of size length(x) by length(y) by length(f) by ..., each
+%       value correspoding to the measurement made at the coordinate and
+%       frequency corresponding to the same indices in the inputs x, y, f.
+%       The matrix S may have any number of extra dimensions, and each of
+%       S(:, :, :, ii) will be treated as an independent data set.
+%   x - Vector of sample x-coordinates.
+%   y - Vector of sample y-coordinates.
+%   y - Vector of desired z-coordinates.
+%   f - Vector of sample frequency coordinates.
+% Outputs:
+%   Img - Matrix of size length(x) by length(y) by length(z) by ..., each
+%       value correspoding to the measurement made at the coordinate
+%       corresponding to the same indices in the inputs x, y, z. The 4th
+%       dimensions and onward will be the same size as the input S.
+%       Essentially, Img(:, :, :, ii) will be the SAR image of the data set
+%       S(:, :, :, ii).
+% Named Arguments:
+%   ZeroPadPercent (0) - Amount of zero padding to use on the x- and
+%       y-axes. Expressed as a percentage of the input size. For example, a
+%       value of 100 will result in the input matrix size being doubled
+%       along both the x- and y-axes.
+%   ZeroPadPercentX (ZeroPadPercent) - Same as ZeroPadPercent, but
+%       specified for the x-axis only.
+%   ZeroPadPercentY (ZeroPadPercent) - Same as ZeroPadPercent, but
+%       specified for the y-axis only.
+%   RemoveAverage (true) - If true, the average value for each xy-slice in
+%       the input S will be removed (e.g., S = S - mean(S, [1, 2]);).
+%   SpeedOfLight (299.792458) - Value of speed of light to use.
+%   Er (1) - Vector of dielectric constants for each layer.
+%   Thk (inf) - Vector of thicknesses for each layer. Thk(end) is assumed
+%       to be inf (i.e., infinite half-space). Must be same lenth as Er.
+%
+% Author: Matt Dvorsky
 
 %% Check Inputs
 arguments
@@ -48,14 +90,14 @@ zLayerStart = [0; cumsum(options.Thk(1:end - 1))];  % Start of each layer
 
 %% Remove Average over Frequency
 if options.RemoveAverage
-    S = S - mean(mean(S, 1), 2);
+    S = S - mean(S, [1, 2]);
 end
 
 %% Calculate Wavenumbers
 zx = 2 * round((options.ZeroPadPercentX ./ 100) .* length(x));
 zy = 2 * round((options.ZeroPadPercentY ./ 100) .* length(y));
 
-k0(1, 1, :) = (2*pi) .*f ./ options.SpeedOfLight;
+k0(1, 1, :) = (2*pi) .* f ./ options.SpeedOfLight;
 
 dx = 1;
 if numel(x) > 1
