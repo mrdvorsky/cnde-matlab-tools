55795cfe56cffec16223f9cdab887db3097a36e6
Matthew Dvorsky <mdvorsky@iastate.edu>
2022-09-27 22:58:43 -0500

    Removed CST files, as they do not work.

--- CST/CST_MATLAB_Control/OpenAndExport.m
+++
@@ -1,4 +0,0 @@
-clc; clear all; close all;
-[file,path] = uigetfile("*.cst");
-[cst,mws] = openCST([path file]);
-S = returnS(mws,1); % this simulation has 1 port
--- CST/CST_MATLAB_Control/saveAsCST.m
+++
@@ -1,3 +0,0 @@
-function saveAsCST(mws,filename,results)
-    mws.invoke('SaveAs',filename,results);   
-end
