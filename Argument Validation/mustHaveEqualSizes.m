1918763d87818022b076dc08a2a9e4c1794e50c6
Matthew Dvorsky <mdvorsky@iastate.edu>
2023-04-24 11:46:27 -0500

    Added "Dimensions" optional argument for the "mustHaveEqualSizes" function.

--- Argument Validation/mustHaveEqualSizes.m
+++ Argument Validation/mustHaveEqualSizes.m
@@ -1,12 +1,13 @@
-function [] = mustHaveEqualSizes(Arrays)
+function [] = mustHaveEqualSizes(Arrays, options)
 %MUSTHAVEEQUALSIZES Validate that input arrays have equal sizes.
-% Throws an error if the input arrays do not have equal sizes.
+% Throws an error if the input arrays do not have equal sizes. Optionally,
+% only checks specified dimensions for equality.
 %
 % Example Usage:
 %   arguments
 %       Array1;
-%       Array2 {mustHaveEqualSizes(Array1, Array2)};
-%       Array3 {mustHaveEqualSizes(Array1, Array3)};
+%       Array2 {mustHaveEqualSizes(Array1, Array2)};    % Check all dimensions.
+%       Array3 {mustHaveEqualSizes(Array1, Array3, Dimenions=[1, 3])};
 %   end
 %
 %   arguments (Repeating)
@@ -19,21 +20,37 @@ function [] = mustHaveEqualSizes(Arrays)
 arguments (Repeating)
     Arrays;
 end
+arguments
+    options.Dimensions(1, :) {mustBeValidDimension} = "all";
+end
 
 %% Check Sizes
 maxInputDim = max(cellfun(@(x) ndims(x), Arrays));
+if strcmp(options.Dimensions, "all")
+    options.Dimensions = 1:maxInputDim;
+end
+maxInputDim = max(maxInputDim, max(options.Dimensions));
+
 inputDims = cell2mat(...
     cellfun(@(x) size(x, 1:maxInputDim).', Arrays, UniformOutput=false)).';
 outputDims = max(inputDims, [], 1);
 
 dimMismatch = any(inputDims ~= outputDims, 1);
-if any(dimMismatch)
+if any(dimMismatch(options.Dimensions))
     inputDimsString = string(inputDims);
     inputDimsString(:, dimMismatch) = ...
         strcat("*", inputDimsString(:, dimMismatch), "*");
 
+    if isequal(options.Dimensions, 1:maxInputDim)
+        dimString = "";
+    else
+        dimString = sprintf(" along dimensions (%s)", ...
+            join(string(options.Dimensions), ", "));
+    end
+
     throwAsCaller(MException("MATLAB:mustHaveEqualSizes", ...
-        "Arguments must have equal sizes:%s%s%s", newline(), newline(), ...
+        "Arguments must have equal sizes%s:%s%s%s", ...
+        dimString, newline(), newline(), ...
         join(compose("    size(arg%d) == [%s]", ...
         (1:size(inputDims, 1)).', ...
         join(inputDimsString, ", ", 2)), ...
