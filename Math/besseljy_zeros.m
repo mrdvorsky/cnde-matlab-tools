e78b94df2d8fcf93a6ed96c70b72f56f58d3045b
Matthew Dvorsky <mdvorsky@iastate.edu>
2025-01-15 17:15:46 -0600

    Fixed 'bugs' with besseljy_zeros.m and besseljyprime_zeros.m, where the sign can change arbitrarily, these functions now have a consistent sign. Added demos.

--- Math/besseljy_zeros.m
+++ Math/besseljy_zeros.m
@@ -5,6 +5,18 @@ function [k, alpha, beta] = besseljy_zeros(nu, n, r1, r2)
 % are both equal to zero. Specifically, this function computes the smallest
 % "n" values of "k".
 %
+% Note that there is an arbitrary scale factor that must be chosen when
+% choosing alpha and beta, similarly to if we were finding zeros of 
+% "a*cos + b*sin". In that case, we would want the scale to be such that
+% the result is a shifted cos function. This function attempts to do the
+% same, so that besseljy returns a "shifted besselj". More specifically,
+% the asymtotic behavior at infinity will be "sqrt(2/(pi*x)) cos(x - c)",
+% where "c" is the required phase shift. This matches the behavior of
+% besselj and bessely.
+%
+% The sign, which is also arbitrary, will be chosen such that the
+% derivative at k*r2 is positive.
+%
 % Example Usage:
 %   [k, alpha, beta] = besseljy_zeros(nu, n, r1, r2);
 %   assert(all(besseljy(alpha, beta, nu, k*r1) == 0));  % Almost passes.
@@ -37,7 +49,7 @@ fun = @(y) besselj(nu, abs(y)).*bessely(nu, (r1./r2).*abs(y)) ...
 k = zeros(n, 1);
 k(1) = abs(fzero(fun, (nu + (nu == 0))));
 for ii = 2:n
-    k_guess = k(ii - 1) + (0.5*pi ./ (1 - (r1./r2)))*[0.9, 1.1];
+    k_guess = k(ii - 1) + (0.5*pi ./ (1 - (r1./r2)./(nu + 1)))*[0.9, 1.1];
     while sum(sign(fun(k_guess))) ~= 0
         k_guess(2) = 1.1*k_guess(2) - 0.1*k(ii - 1);
     end
@@ -53,6 +65,12 @@ for ii = 1:numel(k)
         besseljy(0, 1, nu, k(ii) .* r2)]);
     alpha(ii) = ab(1);
     beta(ii) = ab(2);
+
+    % Choose sign.
+    der_sign = (-1).^(ii) ...
+        .* sign(besseljyprime(alpha(ii), beta(ii), nu, k(ii)*r2));
+    alpha(ii) = der_sign * alpha(ii);
+    beta(ii) = der_sign * beta(ii);
 end
 
 end
