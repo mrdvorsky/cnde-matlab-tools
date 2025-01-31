cedc1dbc9f5e800f0b53c9e3eb4029b2f557ec14
Matthew Dvorsky <mdvorsky@iastate.edu>
2025-01-30 19:09:24 -0600

    Changed phase convention to e^-jwt for animated plots. Also, modified function to properly handle multiple subplots on a single figure.

--- SAR Utilities/showImage.m
+++ SAR Utilities/showImage.m
@@ -1,11 +1,8 @@
-function [varargout] = showImage(x, y, ImgIn, options, imagescOptions)
+function [varargout] = showImage(x, y, ImgIn, options)
 %SHOWIMAGE Shows an xy image.
 % This function is very similar to imagesc(), except that it transposes and
 % squeezes input ImgIn, and it optionally formats a complex image for
-% diplay. It also sets the axis aspect ratio properly. Otherwise, the
-% functionality is very similar to imagesc. Name value pair arguments are
-% passed through to imagesc, and any output arguments of imagesc are
-% forwarded.
+% display. It also sets the axis aspect ratio properly.
 %
 % Example Usage:
 %   showImage(x, y, Img);
@@ -14,33 +11,40 @@ function [varargout] = showImage(x, y, ImgIn, options, imagescOptions)
 %   showImage(x, y, Img, DisplayFormat="Magnitude", Normalize=true);
 %   showImage(x, y, Img, DisplayFormat="MagPhase", PhaseMultiplier=2);
 %
-% If the DisplayFormat option is set to "MagPhase", the image will be shown
-% using color to represent phase, and intesity to represent magnitude. The
-% named options NormalizeFactor and PhaseMultiplier are scale factors for
-% the image intensity and phase colormap, respectively, in this case.
+% If the "DisplayFormat" option is set to "MagPhase", the image will be
+% shown using color to represent phase, and intesity to represent
+% magnitude. The named options NormalizeFactor and PhaseMultiplier are
+% scale factors for the image intensity and phase colormap, respectively,
+% in this case.
+%
+% If the "DisplayFormat" option is set to "Animated", a time changing
+% image will be shown. This will be similar to the "Real" or "Imag"
+% options, but the current phase will change using the e^-jwt convention.
+% This is useful to show electric or magnetic field plots with radiation.
 %
 % Inputs:
 %   x - vector of coordinates of ImgIn (first dimension).
 %   y - vector of coordinates of ImgIn (first dimension).
 %   ImgIn - Array containing image. Should have at most 2 non-singleton
-%       dimensions. Can be complex if DisplayFormat is specified.
+%       dimensions. Can be complex.
 %
 % Outputs:
 %   Passed through from "imagesc". See imagesc documentation for details.
 %
-% Named Options:
-%   DisplayFormat ("None") - For a complex-valued image, this argument
-%       specifies which component to show. Options are "None", "Magnitude",
-%       "dB", "Phase", "Real", "Imag", "MagPhase".
+% Named Arguments:
+%   DisplayFormat ("Real") - For a complex-valued image, this argument
+%       specifies which component to show. Options are "Magnitude",
+%       "dB", "Phase", "Real", "Imag", "RealAbs", "ImagAbs", "MagPhase",
+%       and "Animated".
 %   Normalize (false) - If true, normalize image before showing.
 %   NormalizeFactor (1) - If Normalize=true or DisplayFormat="MagPhase",
 %       this scaling factor is applied post-normalization.
-%   ColorScaleRangeDB (60) - Range, in dB to plot when in 'dB' mode.
+%   ColorScaleRangeDB (60) - Range, in dB to plot when in any 'dB' mode.
 %   PhaseMultiplier (1) - If DisplayFormat="MagPhase", this scale factor is
 %       applied to the phase before displaying.
 %   ShowColorbar (true) - Whether or not to display colorbar.
 %   ColorbarLabel - If specified, use as the colorbar label string.
-%   Axes (gca()) - Axis on which to plot.
+%   Axis (gca()) - Axis on which to plot.
 %
 % Author: Matt Dvorsky
 
@@ -56,18 +60,17 @@ arguments
     options.NormalizeFactor(1, 1) {mustBePositive} = 1;
     options.ColorScaleRangeDB(1, 1) {mustBePositive} = 60;
     options.PhaseMultiplier(1, 1) {mustBePositive} = 1;
+    
     options.ShowColorbar(1, 1) logical = true;
     options.ColorbarLabel {mustBeTextScalar};
-    options.Axes(1, 1) {mustBeA(options.Axes, "Axes")};
+    options.Axis(1, 1) {mustBeA(options.Axis, "Axis")};
 
     options.ShowDisplayFormatMenu(1, 1) logical = true;
+
     options.AnimationFPS(1, 1) ...
         {mustBeInRange(options.AnimationFPS, 0, 20, "exclude-lower")} = 20;
     options.AnimationPeriodSeconds(1, 1) ...
         {mustBeInRange(options.AnimationPeriodSeconds, 0.5, 10)} = 2;
-    
-    imagescOptions.AlphaData;
-    imagescOptions.AlphaDataMapping;
 end
 
 %% Show Image
@@ -79,26 +82,24 @@ if ~isfield(options, "DisplayFormat")
     options.DisplayFormat = "Real";
 end
 
-if ~isfield(options, "Axes")
-    options.Axes = gca;
+if ~isfield(options, "Axis")
+    options.Axis = gca;
 end
 
 maxImgAbs = max(abs(ImgIn(:)));
 if any(strcmp(options.DisplayFormat, ["MagPhase", "Animated"]))
-    [varargout{1:max(nargout, 1)}] = imagesc(options.Axes, x, y, ...
+    [varargout{1:max(nargout, 1)}] = imagesc(options.Axis, x, y, ...
         options.PhaseMultiplier .* rad2deg(angle(ImgIn)).', ...
-        imagescOptions, ...
         AlphaData=convertImage(ImgIn.', options, maxImgAbs));
 else
-    [varargout{1:max(nargout, 1)}] = imagesc(options.Axes, x, y, ...
-        convertImage(ImgIn.', options, maxImgAbs), ...
-        imagescOptions);
+    [varargout{1:max(nargout, 1)}] = imagesc(options.Axis, x, y, ...
+        convertImage(ImgIn.', options, maxImgAbs));
 end
 
-axis(options.Axes, "image");
-axis(options.Axes, "xy");
-set(options.Axes, Color="k");
-setColormap(options.Axes, options, maxImgAbs);
+axis(options.Axis, "image");
+axis(options.Axis, "xy");
+set(options.Axis, Color="k");
+setColormap(options.Axis, options, maxImgAbs);
 
 %% Show Colorbar
 if options.ShowColorbar
@@ -108,76 +109,86 @@ if options.ShowColorbar
         colorbarLabel = getColorbarLabel(options.DisplayFormat);
     end
     
-    colorbarHandle = colorbar(options.Axes);
+    colorbarHandle = colorbar(options.Axis);
     colorbarHandle.Label.String = colorbarLabel;
 end
 
 %% Add UI to Allow Changing Colormap
-if options.ShowDisplayFormatMenu
-    fig = options.Axes.Parent;
-    if ~isa(fig, "matlab.ui.Figure")
-        warning("Parent object of the current axis is not a figure. " + ...
-            "Complex format menu will be disabled.");
-    else
-        menu = uimenu(fig, Text="Display Format");
-        
-        displayItems = [...
-            "Magnitude", ...
-            "dB", ...
-            "Real", ...
-            "Imag", ...
-            "RealAbs", ...
-            "ImagAbs", ...
-            "Phase", ...
-            "MagPhase", ...
-            "Animated" ...
-            ];
+fig = options.Axis.Parent;
+if options.ShowDisplayFormatMenu && ~isa(fig, "matlab.ui.Figure")
+    warning("Parent object of the current axis is not a figure. " + ...
+        "Complex format menu will be disabled.");
+    options.ShowDisplayFormatMenu = false;
+end
 
-        for ii = 1:numel(displayItems)
-            item = uimenu(menu, Text=displayItems(ii), ...
-                MenuSelectedFcn=@displayFormatUpdateFun);
-            if strcmp(displayItems(ii), options.DisplayFormat)
-                item.Checked = "on";
-            end
-        end
-        item = uimenu(menu, Text="Normalize", Separator="on", ...
-            MenuSelectedFcn=@normalizeUpdateFunction);
-        if options.Normalize
+if ~options.ShowDisplayFormatMenu && strcmp(options.DisplayFormat, "Animated")
+    warning("The 'DisplayFormat' argument was set to 'Animated', " + ...
+        "but the 'ShowDisplayFormatMenu' argument was 'false'. " + ...
+        "The display format will be set to 'MagPhase' instead.")
+end
+
+if options.ShowDisplayFormatMenu
+    menu = uimenu(fig, Text="Display Format");
+
+    displayItems = [...
+        "Magnitude", ...
+        "dB", ...
+        "Real", ...
+        "Imag", ...
+        "RealAbs", ...
+        "ImagAbs", ...
+        "Phase", ...
+        "MagPhase", ...
+        "Animated" ...
+        ];
+
+    for ii = 1:numel(displayItems)
+        item = uimenu(menu, Text=displayItems(ii), ...
+            MenuSelectedFcn={@displayFormatUpdateFun, options.Axis});
+        if strcmp(displayItems(ii), options.DisplayFormat)
             item.Checked = "on";
         end
+    end
+    item = uimenu(menu, Text="Normalize", Separator="on", ...
+        MenuSelectedFcn={@normalizeUpdateFunction, options.Axis});
+    if options.Normalize
+        item.Checked = "on";
+    end
 
-        dataStruct.Img = ImgIn;
-        dataStruct.PlotHandle = varargout{1};
-        dataStruct.options = options;
-        dataStruct.maxImgAbs = maxImgAbs;
-        
-        % Set up animation.
-        animMapSize = 256;
+    dataStruct.Img = ImgIn;
+    dataStruct.PlotHandle = varargout{1};
+    dataStruct.options = options;
+    dataStruct.maxImgAbs = maxImgAbs;
 
-        animationColorMap = interp1( ...
-            linspace(-1, 1, 1024), ...
-            colormapplusminus(1024), ...
-            cosd(linspace(-180, 180, animMapSize)));
-        
-        shiftCountPerUpdate = animMapSize ...
-            ./ (options.AnimationPeriodSeconds .* options.AnimationFPS);
-        dataStruct.animationTimer = timer(...
-            Name=sprintf("Fig_%d_timer", fig.Number), ...
-            ExecutionMode="fixedRate", ...
-            Period=0.001*round(1000/options.AnimationFPS), ...
-            TimerFcn={@animationTimerFunction, ...
-                options.Axes, animationColorMap, shiftCountPerUpdate});
-        
-        fig.CloseRequestFcn = @closeRequest;
+    % Set up animation.
+    animMapSize = 256;
 
-        % Store data in figure.
-        guidata(fig, dataStruct);
+    animationColorMap = interp1( ...
+        linspace(-1, 1, 1024), ...
+        colormapplusminus(1024), ...
+        cosd(linspace(-180, 180, animMapSize)));
 
-        if strcmp(options.DisplayFormat, "Animated")
-            start(dataStruct.animationTimer);
-        end
+    shiftCountPerUpdate = -animMapSize ...
+        ./ (options.AnimationPeriodSeconds .* options.AnimationFPS);
+    dataStruct.animationTimer = timer(...
+        Name=sprintf("Fig_%d_timer", fig.Number), ...
+        ExecutionMode="fixedRate", ...
+        Period=0.001*round(1000/options.AnimationFPS), ...
+        TimerFcn={@animationTimerFunction, ...
+        options.Axis, animationColorMap, shiftCountPerUpdate});
+
+    options.Axis.DeleteFcn = @closeRequest;
+
+    % Store data in figure.
+    options.Axis.UserData = dataStruct;
+
+    if strcmp(options.DisplayFormat, "Animated")
+        start(dataStruct.animationTimer);
     end
 end
+
+
+
 
 end
 
@@ -186,13 +197,17 @@ end
 
 
 %% Display Format Update Function
-function displayFormatUpdateFun(eventSource, ~)
+function displayFormatUpdateFun(eventSource, ~, axis)
+    if eventSource.Checked == "on"
+        return;
+    end
+
     for ii = 2:numel(eventSource.Parent.Children)
         eventSource.Parent.Children(ii).Checked = "off";
     end
     eventSource.Checked = "on";
 
-    updateData = guidata(eventSource);
+    updateData = axis.UserData;
     updateData.options.DisplayFormat = eventSource.Text;
 
     if ~strcmp(updateData.options.DisplayFormat, "Animated")
@@ -206,21 +221,21 @@ function displayFormatUpdateFun(eventSource, ~)
         start(updateData.animationTimer);
     end
     
-    guidata(eventSource, updateData);
+    axis.UserData = updateData;
 end
 
-function normalizeUpdateFunction(eventSource, ~)
+function normalizeUpdateFunction(eventSource, ~, axis)
     if eventSource.Checked == "on"
         eventSource.Checked = "off";
     else
         eventSource.Checked = "on";
     end
 
-    updateData = guidata(eventSource);
+    updateData = axis.UserData;
     updateData.options.Normalize = (eventSource.Checked == "on");
     updateDisplayFormat(updateData.Img, updateData.PlotHandle, ...
         updateData.options, updateData.maxImgAbs);
-    guidata(eventSource, updateData);
+    axis.UserData = updateData;
 end
 
 function updateDisplayFormat(Img, PlotHandle, options, maxImgAbs)
@@ -232,24 +247,24 @@ function updateDisplayFormat(Img, PlotHandle, options, maxImgAbs)
         PlotHandle.AlphaData = 1;
     end
 
-    axes = PlotHandle.Parent;
-    setColormap(axes, options, maxImgAbs);
-    if ~isempty(axes.Colorbar)
-        axes.Colorbar.Label.String = getColorbarLabel(options.DisplayFormat);
+    axis = options.Axis;
+    setColormap(axis, options, maxImgAbs);
+    if ~isempty(axis.Colorbar)
+        axis.Colorbar.Label.String = getColorbarLabel(options.DisplayFormat);
     end
 end
 
 %% Window Animation Functions
-function closeRequest(eventSource, ~)
+function closeRequest(axis, ~)
     try
-        figureData = guidata(eventSource);
+        figureData = axis.UserData;
         stop(figureData.animationTimer);
         delete(figureData.animationTimer);
     catch ex
-        delete(eventSource);
+        delete(axis);
         rethrow(ex);
     end
-    delete(eventSource);
+    delete(axis);
 end
 
 function animationTimerFunction(eventSource, ~, axis, animColor, shiftCountPerUpdate)
@@ -316,7 +331,7 @@ function colorbarLabel = getColorbarLabel(displayFormat)
     end
 end
 
-function setColormap(axes, options, maxVal)
+function setColormap(axis, options, maxVal)
     if options.Normalize
         maxVal = options.NormalizeFactor;
     end
@@ -326,32 +341,32 @@ function setColormap(axes, options, maxVal)
 
     switch options.DisplayFormat
         case "Magnitude"
-            colormap(axes, "jet");
-            clim([0, maxVal]);
+            colormap(axis, "jet");
+            clim(axis, [0, maxVal]);
         case "dB"
-            colormap(axes, "jet");
-            clim([-options.ColorScaleRangeDB, 0] + db(maxVal));
+            colormap(axis, "jet");
+            clim(axis, [-options.ColorScaleRangeDB, 0] + db(maxVal));
         case "Real"
-            colormap(axes, "colormapplusminus");
-            clim([-maxVal, maxVal]);
+            colormap(axis, "colormapplusminus");
+            clim(axis, [-maxVal, maxVal]);
         case "Imag"
-            colormap(axes, "colormapplusminus");
-            clim([-maxVal, maxVal]);
+            colormap(axis, "colormapplusminus");
+            clim(axis, [-maxVal, maxVal]);
         case "RealAbs"
-            colormap(axes, "colormapplusminusabs");
-            clim([-maxVal, maxVal]);
+            colormap(axis, "colormapplusminusabs");
+            clim(axis, [-maxVal, maxVal]);
         case "ImagAbs"
-            colormap(axes, "colormapplusminusabs");
-            clim([-maxVal, maxVal]);
+            colormap(axis, "colormapplusminusabs");
+            clim(axis, [-maxVal, maxVal]);
         case "Phase"
-            colormap(axes, "hsv");
-            clim([-180, 180]);
+            colormap(axis, "hsv");
+            clim(axis, [-180, 180]);
         case "MagPhase"
-            colormap(axes, "hsv");
-            clim([-180, 180]);
+            colormap(axis, "hsv");
+            clim(axis, [-180, 180]);
         case "Animated"
-            colormap(axes, "hsv");
-            clim([-180, 180]);
+            colormap(axis, "hsv");
+            clim(axis, [-180, 180]);
         otherwise
             error("'DisplayFormat' argument '%s' is not recognized", ...
                 options.DisplayFormat);
