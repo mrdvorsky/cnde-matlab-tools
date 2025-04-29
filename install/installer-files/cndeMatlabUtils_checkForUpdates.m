07c68e3470cc41a3b2b4c7d5968ccb99cf05e1bf
Matthew Dvorsky <matthew.ryan.dvorsky@gmail.com>
2025-04-28 19:17:23 -0500

    Moved files that need to be copied to a separate directory.

--- install/installer-files/cndeMatlabUtils_checkForUpdates.m
+++ install/installer-files/cndeMatlabUtils_checkForUpdates.m
@@ -1,8 +1,12 @@
-function [] = cndeMatlabUtils_update()
+function [] = cndeMatlabUtils_checkForUpdates()
 %This function "updates" the cnde-matlab-utils library.
 % Essentially, this function does the following:
+%   - Pull
 %
 % Author: Matt Dvorsky
+
+
+repo = gitrepo(fullfile(userpath(), "cnde-matlab-utils"));
 
 
 end
